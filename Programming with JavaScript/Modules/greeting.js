495ed0ed985f8e57af62f383fbcc18bf790e92f4
jl-molina <joseluisml1218@gmail.com>
2022-07-14 11:41:00 -0600

    feat: Programming with JavaScript - JavaScript Modules

---
+++ Programming with JavaScript/Modules/greeting.js
@@ -0,0 +1,13 @@
+export function informalGreeting(name) {
+    console.log(`Hello, ${name}`);
+}
+
+export function formalGreeting(name) {
+    console.log(`Good day, ${name}`)
+}
+
+function greeting() {
+    console.log('Hello');
+}
+
+export default greeting;
